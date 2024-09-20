ba2e6a7af16c5db6233a25f743da87eae539df30
Shanzariaz56 <148033648+Shanzariaz56@users.noreply.github.com>
2024-09-20 15:44:51 +0100

    Update models.py

--- stock_project/stockApp/models.py
+++ stock_project/stockApp/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 
 '''Here is the model for the user with a username and initial balance
+ Fields:
+        - username: A name for the user.
+        - balance: The initial balance of the user
 '''
 class User(models.Model):
     username= models.CharField(max_length=100)
     Initial_balance=models.IntegerField()
 '''
 Here is the model for the stock with ticker and price the "ticker" is unique and combination of alphabet,letter etc
+Fields:
+        - ticker: The stock ticker symbol
+        - price: The current price of the stock.
 '''
 class Stock(models.Model):
     ticker=models.CharField(max_length=50, unique=True)
@@ -16,6 +22,13 @@ class Stock(models.Model):
     
 '''
 Here is the model for the transaction user, ticker, transaction_type, and transaction_volume, transaction_price, timestamp as inputs
+Fields:
+        - user: The user who performed the transaction (ForeignKey User).
+        - ticker: The stock involved in the transaction (ForeignKey Stock).
+        - transaction_type: The type of transaction, 'buy' or 'sell'.
+        - transaction_volume: The number of stocks quantity involved in the transaction.
+        - transaction_price: The total price of the transaction.
+        - created_at: The time when the transaction was created.
 '''
 #first make a transaction type class
 class TransactionType(models.TextChoices):
